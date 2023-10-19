61574d0ba7666bda8292426cf19a623a1be07971
Armando Rochin <armando.rochin@gmail.com>
2023-10-19 13:29:58 -0700

    Se agregaron las siguientes dependencias:
    	- Activity:1.8.0
    	- viewmodel:2.6.2
    	- livedata:2.6.2
    	- coroutines:1.7.1
    	- hilt:2.48.1
    	- retrofit:2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -44,13 +45,25 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    //Activity
+    implementation("androidx.activity:activity-ktx:1.8.0")
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    //Livedata
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    //corutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    //Dagger HILT
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
+
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
